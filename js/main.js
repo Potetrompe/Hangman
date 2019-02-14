b1f73e373ff3afe488676b2eb28d67483c9512c2
Potetrompe <46726271+Potetrompe@users.noreply.github.com>
2019-02-14 17:59:08 +0100

    first push

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//* Canvas setup
+const canvasGfx = document.getElementById("canvasGfx");
+const ctx = canvasGfx.getContext("2d");
+
+canvW = document.body.offsetWidth   * 1/2;
+canvH = document.body.offsetHeight  * 2/3;
+canvasGfx.width = canvW;
+canvasGfx.height = canvH;
+
+
+//? Psudo code:
+
+
+
+//* Globals
+const mainCol = "#eff";
+
